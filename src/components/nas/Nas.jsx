7acc8939ec845f67baeddd1938fbc4a20ed3bf9c
elbek1307 <elbekjonabdushukurov1307@gmail.com>
2026-06-05 00:12:57 +0500

    nas jsx qaytatdan yozildi

--- src/components/nas/Nas.jsx
+++ src/components/nas/Nas.jsx
@@ -2,46 +2,35 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './Nas.css';
 
-// Rasmingiz manzilini shu yerga yozasiz
-import nasImg from './nasimg.jpg'; 
+// Rasm manzilini o'zingiz to'g'rilab olasiz
+import nasImg from './nas-img.jpg'; 
 
 const Nas = () => {
-    const { t, i18n } = useTranslation();
+    // useTranslation() funksiya ko'rinishida chaqirildi
+    const { t } = useTranslation();
 
     return (
         <section className="nas">
             <div className="container">
-                <div className="nas_wrapper">
+                {/* 1. Rasm */}
+                <img src={nasImg} alt="О нас" className="nas_img" />
+                
+                {/* 2. Matnlar qismi */}
+                <div className="nas_content">
+                    <span className="nas_subtitle">{t('nas.subtitle')}</span>
                     
-                    {/* Chap taraf - Rasm */}
-                    <div className="nas_left">
-                        <img src={nasImg} alt="О нас" className="nas_img" />
-                    </div>
+                    <h2 className="nas_title">
+                        <span className="nas_title_green">{t('nas.title_green')}</span><br />
+                        {t('nas.title_main')}
+                    </h2>
+                    
+                    <p className="nas_text">{t('nas.desc1')}</p>
+                    <p className="nas_text">{t('nas.desc2')}</p>
+                    <p className="nas_text">{t('nas.desc3')}</p>
 
-                    {/* O'ng taraf - Matnlar */}
-                    <div className="nas_right">
-                        <span className="nas_subtitle">{t('nas.subtitle')}</span>
-                        <h2 className="nas_title">
-                            <span className="nas_title_green">{t('nas.title_green')}</span><br />
-                            {t('nas.title_main')}
-                        </h2>
-                        
-                        <div className="nas_texts">
-                            <p>{t('nas.desc1')}</p>
-                            <p>{t('nas.desc2')}</p>
-                            <p>{t('nas.desc3')}</p>
-                        </div>
-
-                        {/* Tugma / Link */}
-                        <div className="nas_link_wrapper">
-                            <a href="#" className="nas_link">
-                                {t('nas.link')} 
-                                {/* O'q (arrow) uchun oddiy belgi ishlatildi, xohlasangiz SVG qo'ysangiz ham bo'ladi */}
-                                <span className="nas_arrow">⟶</span>
-                            </a>
-                        </div>
-                    </div>
-
+                    <a href="#" className="nas_link">
+                        {t('nas.link')} <span className="nas_arrow">⟶</span>
+                    </a>
                 </div>
             </div>
         </section>
