3952148f5ee7f361ac5045cb9fb2c9023a14e20c
elbek1307 <elbekjonabdushukurov1307@gmail.com>
2026-06-05 00:06:54 +0500

    nas app.jsx ga ulandi

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Header } from './components/Header/Header'
 import { Hero } from './components/Hero/Hero'
 import Ser from './components/ser/ser'
 import Courses from './components/Kurs/Courses'
+import Nas from './components/nas/Nas'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Hero />
       <Ser />
       <Courses />
+      <Nas />
     </>
   )
 }
